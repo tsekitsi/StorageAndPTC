c9cf6739da9dbad5437e3d62e68f6009b04ada43
Marios Tsekitsidis <tsekitsi@grinnell.edu>
2019-04-05 11:34:07 -0500

    Reading LHConfig done

--- StorageAndPTC/src/MyLH/LH.java
+++ StorageAndPTC/src/MyLH/LH.java
@@ -6,21 +6,23 @@ import PTCFramework.ConsumerIterator;
 import PTCFramework.PTCFramework;
 import PTCFramework.ProducerIterator;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+//import java.io.BufferedReader;
+//import java.io.File;
+//import java.io.FileNotFoundException;
+//import java.io.FileReader;
+//import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
+import org.json.*;
 
 public class LH {
 	
 	// file-related:
-	private static Integer m = 3;
-	private Integer sP = 0;
-	private Integer numOfPages = 100;
-	private double acl_Min = 1.25;
-	private double acl_Max = 1.5;
+	private static Integer m;
+	private static Integer sP;
+	private static Integer numOfPages;
+	private static double acl_Min;
+	private static double acl_Max;
 	private double acl;
 	
 	// storage-related:
@@ -30,6 +32,7 @@ public class LH {
 	// insert
 	private static void insertTuples(String fileName) {
 		try {
+			// read text file with the records to be inserted to storage:
 			File file = new File(fileName);
 			FileReader fileReader = new FileReader(file);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -50,6 +53,7 @@ public class LH {
 		String[] str = tuple.split(",");
 		int key = Integer.parseInt(str[0]);
 		int result = key%m;
+		// INSERT TO CHAIN INDEXED result
 		System.out.println(Arrays.toString(str));
 	}
 	
@@ -57,22 +61,32 @@ public class LH {
 	
 	// load (?)
 	
-	private void initializeFileSystem() {
-		// initialize LH file:
-		String path_of_LtoPfile = "";
-		createLtoPfile(path_of_LtoPfile);
-		PBFileEntry e = new PBFileEntry();
-		e.setName("MyLHFile");
-		e.setHomePage("-1");
-		e.setLtoP_Map(path_of_LtoPfile);
-		e.setM(m);
-		e.setsP(sP);
-		e.setNumOfPages(numOfPages);
-		e.setACL_Min(acl_Min);
-		e.setACL_Max(acl_Max);
+	private static void initializeFileSystem() {
+		try {
+			// reading the parameters from LHConfig:			
+			String jsonData = readFile("LHConfig.json");
+			JSONObject jobj = new JSONObject(jsonData);
+			
+			String path_of_LtoPfile = jobj.getString("LtoP_File");
+			createLtoPfile(path_of_LtoPfile);
+			
+			// initialize LH file:
+			PBFileEntry e = new PBFileEntry();
+			e.setName(jobj.getString("FileName"));
+			e.setHomePage("homePage");
+			e.setLtoP_Map(path_of_LtoPfile);
+			e.setM(jobj.getInt("M"));
+			e.setsP(jobj.getInt("sP"));
+			e.setNumOfPages(jobj.getInt("NumOfPages"));
+			e.setACL_Min(jobj.getDouble("ACL_Min"));
+			e.setACL_Min(jobj.getDouble("ACL_Max"));
+			e.setACL_Min(jobj.getDouble("ACL"));
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 	}
 
-	private void createLtoPfile(String path) {
+	private static void createLtoPfile(String path) {
 		// TODO Auto-generated method stub
 		
 	}
@@ -81,8 +95,26 @@ public class LH {
 		// TODO writes to serialized logical-to-physical mapping file.
 	}
 	
+	public static String readFile(String filename) {
+	    String result = "";
+	    try {
+	        BufferedReader br = new BufferedReader(new FileReader(filename));
+	        StringBuilder sb = new StringBuilder();
+	        String line = br.readLine();
+	        while (line != null) {
+	            sb.append(line);
+	            line = br.readLine();
+	        }
+	        result = sb.toString();
+	    } catch(Exception e) {
+	        e.printStackTrace();
+	    }
+	    return result;
+	}
+	
 	public static void main(String[] args) throws Exception {
-		insertTuple("Emp.txt");
+		initializeFileSystem();
+		//insertTuple("Emp.txt");
 		/*
 		LH lh = new LH();
 		lh.initializeFileSystem();
